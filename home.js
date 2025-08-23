2fd13329684f1b4e32c444b27d2c74c757b632f4
itzakio <itzakio3@gmail.com>
2025-08-23 17:04:31 +0600

    pin number and account number validation added

--- home.js
+++ home.js
@@ -1,20 +1,36 @@
 document.getElementById('btn-add-money')
 .addEventListener('click', function(e){
     e.preventDefault();
-    console.log('add money clicked')
 
     const bank = document.getElementById('bank').value;
     const accountNumber = document.getElementById('account-number').value;
-    const addAmount = parseInt(document.getElementById('add-amount').value);
+    const addAmountStr = document.getElementById('add-amount').value;
+    const addAmount = parseInt(addAmountStr);
     const pinNumber = document.getElementById('add-pin').value;
     const availableBalance = parseInt(document.getElementById('available-balance').innerText);
     
-    const newBalance = availableBalance + addAmount;
-    document.getElementById('available-balance').innerText = newBalance;
-    document.getElementById('add-amount').value = '';
+    
+    const myPin = '2001';
+    const myAccountNum = '19091291960';
+    
+    if (accountNumber !== myAccountNum){
+        alert('Please provide valid account number')
+    }
+    if (addAmountStr === ''){
+        alert('Please provide add amount')
+    }
+    else if (pinNumber === myPin){
+        const newBalance = availableBalance + addAmount;
+        document.getElementById('available-balance').innerText = newBalance;
+        document.getElementById('add-amount').value = '';
+        document.getElementById('add-pin').value = '';
+    }
+    else {
+        alert('Pin number is invalid')
+    }
 
-    console.log(bank, accountNumber, addAmount, pinNumber);
-    console.log(availableBalance);
+    // console.log(bank, accountNumber, addAmount, pinNumber);
+    // console.log(availableBalance);
     
     
 })
